62433614caa141e68f33d6a11789a5ef2b323eb4
幻弦让叶 <1248389474@qq.com>
2023-07-22 20:57:57 +0800

    新增RadioGroup

---
+++ src/jvmMain/kotlin/ViewRadioGroup.kt
@@ -0,0 +1,109 @@
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SimpleRadioGroup(
+    modifier: Modifier = Modifier,
+    contentModifier: Modifier = Modifier,
+    orientation: Orientation = Orientation.Horizontal,
+    defaultSelected: Int = 0,
+    tabs: List<CustomTab>,
+    onSelected: (Int, CustomTab) -> Unit,
+    content: @Composable (
+        tab: CustomTab,
+        selected: String,
+        childModifier: Modifier
+    ) -> Unit = { _,_,_ -> },
+) {
+    if (tabs.isEmpty()) {
+        return
+    }
+    var selectedTab by remember {
+        mutableStateOf(tabs[defaultSelected].tag)
+    }
+    if (orientation == Orientation.Horizontal) {
+        Row(
+            modifier = modifier
+                .selectableGroup()
+                .horizontalScroll(rememberScrollState()),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ItemView(
+                modifier = contentModifier,
+                selectedTab = selectedTab,
+                tabs = tabs,
+                onSelected = { index, tab ->
+                    selectedTab = tabs[index].tag
+                    onSelected(index, tab)
+                },
+                content = content
+            )
+        }
+    } else {
+        Column(
+            modifier = modifier
+                .selectableGroup()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ItemView(
+                modifier = contentModifier,
+                selectedTab = selectedTab,
+                tabs = tabs,
+                onSelected = { index, tab ->
+                    selectedTab = tabs[index].tag
+                    onSelected(index, tab)
+                },
+                content = content
+            )
+        }
+    }
+}
+
+@Composable
+private fun ItemView(
+    modifier: Modifier,
+    selectedTab: String,
+    tabs: List<CustomTab>,
+    onSelected: (Int, CustomTab) -> Unit,
+    content: @Composable (
+        tab: CustomTab,
+        selected: String,
+        childModifier: Modifier
+    ) -> Unit = { _,_,_ -> },
+) {
+    tabs.forEachIndexed { index, tab ->
+        Column {
+            Box(
+                modifier = modifier
+                    .selectable(
+                        selected = selectedTab == tab.tag,
+                        onClick = {
+                            onSelected(index, tabs[index])
+                        },
+                        role = Role.RadioButton
+                    )
+            ) {
+                content(tab, selectedTab, Modifier.align(Alignment.Center))
+            }
+            if (index != tabs.size - 1) {
+                Spacer(Modifier.height(8.dp))
+            }
+        }
+    }
+}
+
+data class CustomTab(
+    val text: String,
+    val tag: String = text
+)
