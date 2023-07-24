9cafc7a9be8b42542a5d4778b42760825c9b1e5c
幻弦让叶 <1248389474@qq.com>
2023-07-24 20:40:22 +0800

    调整

--- src/jvmMain/kotlin/viewmodel/ShellViewModel.kt
+++ src/jvmMain/kotlin/viewmodel/ShellViewModel.kt
@@ -1,5 +1,6 @@
 package viewmodel
 
+import di.CommonSetting
 import shell.RunCommandState
 import di.ShellProcess
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -10,15 +11,25 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import viewmodel.base.ScopeViewModel
 import viewmodel.base.ScopeViewModelImpl
+import java.io.File
 
 @OptIn(DelicateCoroutinesApi::class)
 class ShellViewModel(
     private val delegate: ScopeViewModelImpl = ScopeViewModelImpl()
 ): ScopeViewModel by delegate {
 
+    companion object {
+
+        private const val cacheTempPathname = "cacheTempPath"
+        private const val cacheTempFilename = "cacheTempApk"
+    }
+
     init {
         provideScope(GlobalScope)
     }
+
+    private val tempPath = CommonSetting.basePath + File.separator + cacheTempFilename + File.separator
+    private val tempApk = tempPath + cacheTempFilename
 
     private val _signState = MutableStateFlow<RunCommandState>(RunCommandState.Idle)
     val signState: StateFlow<RunCommandState> = _signState.asStateFlow()
@@ -34,10 +45,10 @@ class ShellViewModel(
         delegate.scope.launch {
             _signState.tryEmit(RunCommandState.Running)
             ShellProcess.runSign(
-                zipalignPath = "",
-                apksignerPath = "",
+                zipalignPath = CommonSetting.zipalignPath,
+                apksignerPath = CommonSetting.apksignerPath,
                 originFilepath = originFilepath,
-                tempFilepath = "",
+                tempFilepath = tempApk,
                 jksPath = jksPath,
                 alias = alias,
                 outputFile = outputFile,
