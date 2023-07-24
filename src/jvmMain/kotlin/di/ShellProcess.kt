4b9b4a384f72a29693c9a87e24e5f2b73c6b7374
幻弦让叶 <1248389474@qq.com>
2023-07-24 20:40:50 +0800

    调整

--- src/jvmMain/kotlin/di/ShellProcess.kt
+++ src/jvmMain/kotlin/di/ShellProcess.kt
@@ -1,3 +1,5 @@
+package di
+
 import shell.CommandBuilder
 import shell.CommandResult
 import shell.ShellCommand
@@ -14,14 +16,19 @@ object ShellProcess {
         outputFile: String,
         keystorePwd: String,
         keyPwd: String,
+        onSuccess: () -> Unit,
+        onFailed: (msg: String) -> Unit,
     ) {
         val zipalignResult = runZipalign(zipalignPath, originFilepath, tempFilepath)
         if (zipalignResult.code == 0) {
-            println(">>>> zipalign success")
             val signResult = runSign(apksignerPath, jksPath, alias, outputFile, tempFilepath, keystorePwd, keyPwd)
             if (signResult.code == 0) {
-                println(">>>> sign success")
+                onSuccess.invoke()
+            } else {
+                onFailed("签名失败: ${signResult.stderr}")
             }
+        } else {
+            onFailed("对齐失败: ${zipalignResult.stderr}")
         }
     }
 
