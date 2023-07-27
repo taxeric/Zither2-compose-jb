4045658443bd0f5b0e79d8c9d0bf32dfb795b006
幻弦让叶 <1248389474@qq.com>
2023-07-27 20:10:24 +0800

    调整

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,31 +1,60 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.ComposeWindow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import di.CommonSetting
+import screen.SettingsScreen
+import screen.SignScreen
+import screen.SwitchScreen
+import viewmodel.ShellViewModel
 
 @Composable
 @Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun App(
+    composeWindow: ComposeWindow
+) {
+    val vm = remember { ShellViewModel() }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    var selectedIndex by remember {
+        mutableStateOf(0)
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(vertical = 8.dp)
+    ) {
+        SwitchScreen(
+            modifier = Modifier
+                .weight(2f)
+        ) {
+            selectedIndex = it
+        }
+        Box(
+            modifier = Modifier
+                .weight(4f)
+        ) {
+            when (selectedIndex) {
+                0 -> SignScreen(vm, composeWindow)
+                1 -> SettingsScreen(composeWindow)
+            }
         }
     }
 }
 
 fun main() = application {
+    CommonSetting.readSetting()
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        App(this.window)
     }
 }
