6dfa8267b9fa8bfd3dd40b81971baf3d98b01cf8
幻弦让叶 <1248389474@qq.com>
2023-07-28 15:12:56 +0800

    新增配置ViewModel

---
+++ src/jvmMain/kotlin/viewmodel/ConfigViewModel.kt
@@ -0,0 +1,120 @@
+package viewmodel
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import di.CommonSetting
+import entity.JksEntity
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import viewmodel.base.ScopeViewModel
+import viewmodel.base.ScopeViewModelImpl
+import java.io.File
+import java.io.FileOutputStream
+
+/**
+ * 包含本地和运行期间的配置
+ */
+class ConfigViewModel(
+    scope: CoroutineScope,
+    private val delegate: ScopeViewModelImpl = ScopeViewModelImpl()
+): ScopeViewModel by delegate {
+
+    companion object {
+        val jksCacheName = "jksConfig.json"
+    }
+
+    init {
+        provideScope(scope)
+        readJksConfig()
+    }
+
+    /**
+     * 本地保存的签名文件
+     */
+    private val _jksConfigFlow = MutableStateFlow(listOf<JksEntity>())
+    val jksConfigFlow: StateFlow<List<JksEntity>> = _jksConfigFlow.asStateFlow()
+
+    private fun buildJksPath() = CommonSetting.basePath + File.separator + "jks" + File.separator
+
+    fun readJksConfig(
+        path: String = buildJksPath(),
+        filename: String = jksCacheName
+    ) {
+        read<List<JksEntity>>(path, filename) { list ->
+            _jksConfigFlow.tryEmit(list)
+        }
+    }
+
+    fun writeJksConfig(jksEntity: JksEntity) {
+        val cacheKey = jksConfigFlow.value
+        writeJksConfig(
+            list = mutableListOf<JksEntity>().apply {
+                addAll(cacheKey)
+                add(jksEntity)
+            }
+        )
+    }
+
+    fun writeJksConfig(list: List<JksEntity>, append: Boolean = false) {
+        if (list.isEmpty()) {
+            return
+        }
+        write(
+            list,
+            buildJksPath(),
+            jksCacheName,
+            append
+        )
+    }
+
+    private inline fun <reified T: Any> read(
+        filepath: String,
+        filename: String,
+        crossinline onComplete: (T) -> Unit
+    ) {
+        delegate.scope.launch {
+            val json = withContext(Dispatchers.IO) {
+                val file = File(filepath, filename)
+                if (!file.exists()) {
+                    ""
+                } else {
+                    async {
+                        file.readText()
+                    }.await()
+                }
+            }
+            if (json.isNotEmpty()) {
+                val mapper = jacksonObjectMapper()
+                val value = mapper.readValue<T>(json)
+                onComplete.invoke(value)
+            }
+        }
+    }
+
+    private fun write(
+        any: Any,
+        filepath: String,
+        filename: String,
+        append: Boolean = false
+    ) {
+        delegate.scope.launch {
+            withContext(Dispatchers.IO) {
+                val path = File(filepath)
+                if (!path.exists()) {
+                    path.mkdir()
+                }
+                val file = File(path, filename)
+                val mapper = jacksonObjectMapper()
+                val str = mapper.writeValueAsString(any)
+                if (!file.exists()) {
+                    file.createNewFile()
+                    file.writeText(str)
+                } else {
+                    FileOutputStream(file, append).use { it.write(str.toByteArray(Charsets.UTF_8)) }
+                }
+            }
+        }
+    }
+}
