8216905e3939927a969a1e0f6988068dd57549a3
幻弦让叶 <1248389474@qq.com>
2023-07-28 15:11:32 +0800

    调整, 新增构建方法

--- src/jvmMain/kotlin/entity/JksEntity.kt
+++ src/jvmMain/kotlin/entity/JksEntity.kt
@@ -1,5 +1,8 @@
 package entity
 
+import views.BaseRadioTab
+import java.io.File
+
 data class JksEntity(
     val name: String,
     val path: String,
@@ -7,4 +10,35 @@ data class JksEntity(
     val alias: String,
     val ksPwd: String,
     val fileAlias: String = "",
-)
+    val default: Boolean = false,
+): BaseRadioTab(text = name) {
+
+    companion object {
+
+        val default = JksEntity(
+            name = "",
+            path = "",
+            pwd = "",
+            alias = "",
+            ksPwd = "",
+            default = true
+        )
+
+        fun buildEntityByPath(
+            path: String,
+            pwd: String,
+            alias: String,
+            ksPwd: String,
+        ) : JksEntity {
+            val mName = path.substring(path.lastIndexOf(File.separator) + 1, path.length)
+            return JksEntity(
+                name = mName,
+                path = path,
+                pwd = pwd,
+                alias = alias,
+                ksPwd = ksPwd,
+                fileAlias = mName
+            )
+        }
+    }
+}
