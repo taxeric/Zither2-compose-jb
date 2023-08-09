e42e6294abcd972211ef067f72132c6910a56a46
幻弦让叶 <1248389474@qq.com>
2023-08-09 17:47:28 +0800

    调整

--- src/jvmMain/kotlin/shell/CommandBuilder.kt
+++ src/jvmMain/kotlin/shell/CommandBuilder.kt
@@ -1,8 +1,15 @@
 package shell
 
-class CommandBuilder {
+class CommandBuilder(runByCMD: Boolean = false) {
 
     private val builder = mutableListOf<String>()
+
+    init {
+        if (runByCMD) {
+            builder.add("cmd.exe")
+            builder.add("/c")
+        }
+    }
 
     fun append(commandOrValue: String): CommandBuilder {
         builder.add(commandOrValue)
